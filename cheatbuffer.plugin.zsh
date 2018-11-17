a24dc81280d66f8a1f576494ee099f71c8f9d419
ttrnguye <trung.tn.nguyen@oracle.com>
2018-11-17 15:36:53 -0600

    Add _cheatbuffer_cheat command, which uses 'cheat' to display help.

--- cheatbuffer.plugin.zsh
+++ cheatbuffer.plugin.zsh
@@ -54,7 +54,12 @@ cheatbuffer() {
 ### Methods
 ###
 # These are the different functions that are available for cheatbuffer to use to provide help
-# functions should take in the full command (buffer) and output help.
+# functions should take in three variables:
+# BUFFER=$1
+# LBUFFER=$2
+# RBUFFER$3
+# and output help.
+#
 # If no help is output, then cheatbuffer should try the next function
 # https://stackoverflow.com/questions/20361398/bash-array-of-functions
 
@@ -65,14 +70,15 @@ cheatbuffer() {
 # This function tries to get help starting with the first argument
 # and tries the next argument until it can't get anymore help
 _cheatbuffer_help() {
-	local FULL_COMMAND="$1"
+	local BUFFER="$1"
 
-	local WORD_ARRAY=($=FULL_COMMAND)
+	local WORD_ARRAY=($=BUFFER)
 
 	# get the longest substring that has help
 	local CURRENT_COMMAND=''
 	local FINAL_OUTPUT=''
 	for i in $(seq `echo ${#WORD_ARRAY[@]}`); do
+		# get a substring from 0 to the "i"th word
 		CURRENT_COMMAND=${WORD_ARRAY[@]:0:$i}
 		local OUTPUT=$(eval "$CURRENT_COMMAND --help") 2> /dev/null
 		if [ -z "$OUTPUT" ]; then
@@ -83,12 +89,27 @@ _cheatbuffer_help() {
 	echo "$FINAL_OUTPUT"
 }
 
+# Tries to use the "cheat" command
+# i.e. "cheat $CMD"
 _cheatbuffer_cheat() {
+	local BUFFER="$1"
+	local LBUFFER="$2"
+	local RBUFFER="$3"
 
-}
+	# Not sure what the best way to express that cheat is not installed
+	# Right now this will just exit silently and go onto the next function
+	if ! type cheat; then
+		return 0
+	fi
 
-_cheatbuffer_custom() {
+	# get the word that the cursor is over
+	local CMD="${LBUFFER/* /}${RBUFFER/ */}"
+	# If there are any quotes around the command (e.g. 'man) remove them
+	CMD=$(echo "$CMD" | sed 's/^['\''"]*//g; s/['\''"]*$//g')
 
+	local OUTPUT=$(cheat "$CMD") 2> /dev/null
+
+	echo $OUTPUT
 }
 
 bindkey $CHEATBUFFER_KEY_SEQ cheatbuffer
