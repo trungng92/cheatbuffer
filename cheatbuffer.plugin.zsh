9ab394b98fb715b4eb347863f7cbe83788801551
trungng92 <trung.tn.nguyen@oracle.com>
2018-11-25 22:26:40 -0600

    Revert "Hacky bugfix for when --help was sometimes being used as an argument."
    This reverts commit fdb846931a88f7a3bee8cadd6a4d64ebdf0817c9.
    
    Turns out some things don't like --help being passed twice (e.g. 'git config --help --help' vs 'git config --help')

--- cheatbuffer.plugin.zsh
+++ cheatbuffer.plugin.zsh
@@ -106,8 +106,7 @@ _cheatbuffer_help() {
 
 		# Some things (like "man") output their help on stderr
 		# so redirect stderr to stdout and then check the output
-		# We also run --help --help in to deal with the case where the '--help' may have been the argument to a flag
-		local OUTPUT=$(eval "$CURRENT_COMMAND --help --help") 2> /tmp/.cheatbuffer_help_stderr
+		local OUTPUT=$(eval "$CURRENT_COMMAND --help") 2> /tmp/.cheatbuffer_help_stderr
 		if [ -z "$OUTPUT" ]; then
 			OUTPUT=$(cat /tmp/.cheatbuffer_help_stderr)
 		fi
